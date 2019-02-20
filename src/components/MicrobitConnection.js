fcca612099e00c41d6238ee81709d6d086700684
Marianne Magnussen <mariannemagnussen@dhcp-10-24-21-238.wlan.ntnu.no>
2019-02-20 11:20:35 +0100

    manage to connect and get service

--- src/components/MicrobitConnection.js
+++ src/components/MicrobitConnection.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 
+const MBIT_UART_SERVICE = '6e400001-b5a3-f393-e0a9-e50e24dcca9e' //to send TO the microbit
+const MBIT_UART_RX_CHARACTERISTIC = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E' //to send TO the microbit
+const MBIT_UART_TX_CHARACTERISTIC = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'//to receive data FROM the microbit
 
-var uart_device;
-var uart_characteristic;
+const ACCELEROMETERSERVICE_SERVICE_UUID = 'E95D0753-251D-470A-A062-FA1922DFA9A8'.toLowerCase()
+const ACCELEROMETERDATA_CHARACTERISTIC_UUID = "E95DCA4B-251D-470A-A062-FA1922DFA9A8".toLowerCase();
+const ACCELEROMETERPERIOD_CHARACTERISTIC_UUID = "E95DFB24-251D-470A-A062-FA1922DFA9A8".toLowerCase();
 
-const MBIT_UART_SERVICE = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'.toLowerCase(); //to send TO the microbit
-const MBIT_UART_RX_CHARACTERISTIC = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'.toLowerCase(); //to send TO the microbit
-const MBIT_UART_TX_CHARACTERISTIC = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'.toLowerCase(); //to receive data FROM the microbit
+const LEDSERVICE_SERVICE_UUID = "E95DD91D-251D-470A-A062-FA1922DFA9A8".toLowerCase();
+const LEDMATRIXSTATE_CHARACTERISTIC_UUID = "E95D7B77-251D-470A-A062-FA1922DFA9A8".toLowerCase();
+const LEDTEXT_CHARACTERISTIC_UUID = "E95D93EE-251D-470A-A062-FA1922DFA9A8".toLowerCase();
+const SCROLLINGDELAY_CHARACTERISTIC_UUID = "E95D0D2D-251D-470A-A062-FA1922DFA9A8".toLowerCase();
 
 var new_bubble_x_position = 0, new_bubble_y_position = 0, new_bubble_diameter = 1;
 
 //micro:bit BLE UUID UART
 class MicrobitConnection extends Component {
-
     constructor(props) {
         super(props)
         this.connect = this.connect.bind(this)
-        this.onReceiveUART = this.onReceiveUART.bind(this)
         this.disconnect = this.disconnect.bind(this)
-        this.startReadingFromUART = this.startReadingFromUART.bind(this)
-        this.storeDataPoint = this.storeDataPoint.bind(this)
-        this.splitKeyValue = this.splitKeyValue.bind(this)
         this.state = {
-            microbit_rx_tx: []
+            LEDService: null,
+            writeCharacteristicsLEDMatrix: null
         }
     }
 
@@ -31,22 +32,32 @@ class MicrobitConnection extends Component {
             filters: [{
                 namePrefix: 'BBC micro:bit',
             }],
-            optionalServices: [MBIT_UART_SERVICE]
+            optionalServices: [LEDSERVICE_SERVICE_UUID]
         })
             .then(device => {
-                uart_device = device;
-                console.log("device", device);
-                return device.gatt.connect();
+                console.log("Connecting to GATT server...")
+                return device.gatt.connect()
             })
             .then(server => {
-                console.log("server", server)
-                return server.getPrimaryService(MBIT_UART_SERVICE);
+                console.log(" > Found GATT server")
+                let gattServer = server;
+                return gattServer.getPrimaryService(LEDSERVICE_SERVICE_UUID);
             })
             .then(service => {
-                return Promise.all([service.getCharacteristic(MBIT_UART_RX_CHARACTERISTIC),
-                service.getCharacteristic(MBIT_UART_TX_CHARACTERISTIC)])
-            }).then(rxandtx => {
-                this.startReadingFromUART(rxandtx[0], rxandtx[1])
+                console.log("> Found command LED server", service)
+                this.setState({
+                    LEDService: service
+                })
+                return this.state.LEDService.getCharacteristic(LEDMATRIXSTATE_CHARACTERISTIC_UUID);
+            })
+            .then(characteristicLEDMatrix => {
+                console.log(' > Found write characteristic for LED matrix')
+                this.setState({
+                    writeCharacteristicsLEDMatrix: characteristicLEDMatrix
+                })
+              
+                characteristicLEDMatrix.startNotifications();
+                
             })
             .catch(error => {
                 alert("Something went wrong.. ");
@@ -54,52 +65,20 @@ class MicrobitConnection extends Component {
             });
     }
 
-    startReadingFromUART(rxandtx) {
-        rxandtx.subscribeToMessages(this.storeDataPoint);
-    }
-
-    storeDataPoint(message: String) {
-        let kv = this.splitKeyValue(message);
-        let key = kv.key
-        let value = kv.value
-
-        if (key == "x_accel") {
-            console.log("x_accel=" + value);
-            new_bubble_x_position = parseInt(value)
-        } else if (key == "y_accel") {
-            new_bubble_y_position = parseInt(value)
-        } else if (key == "st_accel") {
-            new_bubble_diameter = parseInt(value)
-        } else {
-            console.log("got a key we weren't looking for: " + key)
-        }
-    }
-
-    splitKeyValue(message: String) {
-        let [key, value] = message.trim().split(":");
-        return { key: key, value: value }
-    }
-
-    onReceiveUART(event) {
-        var str_arr = [];
-        for (var i = 0; i < event.target.value.byteLength; i++) {
-            str_arr[i] = event.target.value.getUint8(i);
-        }
-        var str = String.fromCharCode.apply(null, str_arr);
-        console.log('data:' + str);
-        document.js.data.value = str;
-    }
-
     disconnect() {
-        if (!uart_device || !uart_device.gatt.connected) return;
-        uart_device.gatt.disconnect();
+        if (!this.uart_device || !this.uart_device.gatt.connected) return;
+        this.uart_device.gatt.disconnect();
+    }
+
+    setLeds(LEDMatrix) {
+        
     }
 
     render() {
         return (
             <button onClick={this.connect} >
-                Koble til Micro: bit
-        </button>
+                Koble til Micro:bit
+            </button>
         )
     }
 }
